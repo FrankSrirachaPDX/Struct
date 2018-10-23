87898dc1c994ac7e9b78d1cd43a3bdf1f3f7cd3f
Ryan <werner.ryan18@gmail.com>
2018-10-23 13:18:41 -0700

    Add project files.

---
+++ Struct/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            string holder;
+
+            Console.WriteLine("Enter a decimal number!");
+
+            holder = Console.ReadLine();
+            number.Amount = Convert.ToDecimal(holder);
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
